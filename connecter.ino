5c413012309007f958f7af2585c5a9cd85d6ff8b
theekshanaj123 <106722853+theekshanaj123@users.noreply.github.com>
2023-10-01 16:41:26 +0530

    blinking

--- connecter.ino
+++ connecter.ino
@@ -1,16 +1,3 @@
-/*
-  Rui Santos
-  Complete project details at our blog.
-    - ESP32: https://RandomNerdTutorials.com/esp32-firebase-realtime-database/
-    - ESP8266: https://RandomNerdTutorials.com/esp8266-nodemcu-firebase-realtime-database/
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-  Based in the RTDB Basic Example by Firebase-ESP-Client library by mobizt
-  https://github.com/mobizt/Firebase-ESP-Client/blob/main/examples/RTDB/Basic/Basic.ino
-*/
-
-
-
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
 #include <WiFiManager.h>
@@ -28,7 +15,7 @@
 #define API_KEY "AIzaSyAlPBreNK8nZPvmJWDUlB8i9FbBzSqyCtk"
 
 // Insert RTDB URLefine the RTDB URL */
-#define DATABASE_URL "https://b-adb-me-default-rtdb.asia-southeast1.firebasedatabase.app/" 
+#define DATABASE_URL "https://b-adb-me-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 //Define Firebase Data object
 FirebaseData fbdo;
@@ -37,10 +24,18 @@ FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
-int intValue;
+// int intValue;
 float floatValue;
 bool signupOK = false;
 const int LED_PIN = A0;
+unsigned long blinkStartTime;         // Store the start time of the blinking
+unsigned long blinkDuration = 30000;  // Blink duration in milliseconds
+unsigned long ledMillis = 0;
+unsigned long blinkInterval = 1000;  // Blink interval in milliseconds
+bool databaseUpdated = false;
+unsigned long blinkCounter = 0;
+
+
 
 void setup() {
 
@@ -52,7 +47,7 @@ void setup() {
   // Create an instance of WiFiManager
   WiFiManager wifiManager;
 
-   // Connect to Wi-Fi or enter configuration mode
+  // Connect to Wi-Fi or enter configuration mode
   if (!wifiManager.autoConnect("AutoConnectAP")) {
     Serial.println("Failed to connect or configure. Restarting...");
     delay(3000);
@@ -73,19 +68,32 @@ void setup() {
   if (Firebase.signUp(&config, &auth, "", "")) {
     Serial.println("ok");
     signupOK = true;
-  }
-  else {
+  } else {
     Serial.printf("%s\n", config.signer.signupError.message.c_str());
   }
 
   /* Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback;  //see addons/TokenHelper.h
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
 
-  digitalWrite(LED_BUILTIN, LOW);
+  blinkStartTime = millis();  // Initialize the start time
+
+  if (Firebase.ready() && signupOK) {
+    Firebase.RTDB.setInt(&fbdo, "/test/int", 1);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
+
+
+
+unsigned long previousMillis = 0;
+const long interval = 500;  // Blink interval in milliseconds
+int blinkCount = 0;
+bool isBlinking = false;
+int intValue = 0;  // Initialize
+unsigned long startBlinkMillis = 0;
 
 void loop() {
   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 1500 || sendDataPrevMillis == 0)) {
@@ -94,22 +102,48 @@ void loop() {
       if (fbdo.dataType() == "int") {
         intValue = fbdo.intData();
         Serial.println(intValue);
-
-        if(intValue != 1)
-        {
-          Serial.println("intValue");
-          digitalWrite(LED_BUILTIN, HIGH);
+        if (intValue == 0) {
+          digitalWrite(LED_BUILTIN, HIGH);  // Turn off the LED
+          isBlinking = false;
+        } else if (intValue == 1 && !isBlinking) {
+          // Start blinking when intValue is 1 and not already blinking
+          isBlinking = true;
+          blinkCount = 0;
+          previousMillis = millis();
+          startBlinkMillis = millis();
+        } else {
+          Serial.println("Invalid value received from Firebase: " + String(intValue));
         }
-        else{
-          digitalWrite(LED_BUILTIN, LOW);
-        }
+      } else {
+        Serial.println("Unexpected data type received from Firebase.");
       }
-    }
-    else {
-      Serial.println(fbdo.errorReason());
+    } else {
+      Serial.println("Firebase read error: " + fbdo.errorReason());
     }
   }
 
-  Serial.println("other on");
+  // Handle LED blinking
+  if (isBlinking) {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval) {
+      previousMillis = currentMillis;
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  // Toggle LED state
+      blinkCount++;
+      if (blinkCount >= 40) {  // Blink the LED 10 times (20 half-second intervals)
+        isBlinking = false;
+        digitalWrite(LED_BUILTIN, HIGH);  // Turn off the LED
+        // Set Firebase value to 0
+        Firebase.RTDB.setInt(&fbdo, "/test/int", 0);
+        // Firebase.updateNode(&fbdo, "/test/int", 0);
+      }
+    }
 
+    // Check if 1 minute has passed
+    // if (currentMillis - startBlinkMillis >= 60000) {
+    //   isBlinking = false;
+    //   digitalWrite(LED_BUILTIN, HIGH);
+    //   Firebase.RTDB.setInt(&fbdo, "/test/int", 0);
+    //   // Firebase.RTDB.updateNode()updateNode(&fbdo, "/test/int", 0);
+    // }
+  }
 }
